24586a5a03a7b9f151afa4fa69ad2b27ce3f1453
Ancient-R <navalimaricardo@gmail.com>
2020-11-04 18:18:50 -0600

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const showMenu = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+
+showMenu.addEventListener('click', () => menu.classList.toggle('menu--show') );
